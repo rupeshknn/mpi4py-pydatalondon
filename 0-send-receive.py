7779f1dfa4970881e1df10808ba2bace20f34424
rupeshknn <rupeshknn@gmail.com>
2022-09-08 11:22:24 +0530

    changed to somehting

--- 0-send-receive.py
+++ 0-send-receive.py
@@ -15,7 +15,7 @@ rank = comm.Get_rank()             # The process(or) ID
 if rank == 0:
     msg = 'Dear 1, Hi! Love 0.'    # (not in scope in other branch)
     comm.send(msg, dest=1)
-    print(f'Processor {rank} sent message to processor 1: {repr(msg)}')
+    print(f'Processor {rank} something else: {repr(msg)}')
 elif rank == 1:
     msg = comm.recv(source=0)      # Can omit the source to receive from anyone
     print(f'Processor {rank} received message from 0: {repr(msg)}')
